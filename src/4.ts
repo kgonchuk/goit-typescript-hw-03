09f2702fc143ea27f2ad2e732093f280fa4fb34e
kgonchuk <123867028+kgonchuk@users.noreply.github.com>
2023-10-11 11:24:09 +0300

    finish

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,49 @@
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.floor(Math.random() * 10) + 1;
+  }
+  getSignature(): number {
+    return this.signature;
+  }
+}
+class Person {
+  private key: Key;
+  constructor(key: Key) {
+    this.key = key;
+  }
+  getKey(): Key {
+    return this.key;
+  }
+}
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+  comeIn(person: Person) {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log(`Person ${person.getKey().getSignature()} walked into house`);
+    } else {
+      console.log("Door is cloose");
+    }
+  }
+  abstract openDoor(key: Key): void;
+}
+class MyHouse extends House {
+  openDoor(key: Key) {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("Door is open");
+    } else {
+      console.log("door is cloose");
+    }
+  }
+}
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +52,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
